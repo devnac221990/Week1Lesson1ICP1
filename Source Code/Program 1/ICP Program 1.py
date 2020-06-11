894963055ae40ba8aad79f73adcf0dacd4d74c8f
devnac221990 <devnac221990@users.noreply.github.com>
2020-06-10 19:16:56 -0500

    ICP Week 1.1 Code-Screenshot-Videos
    Includes- Code's for the 3 programs for assignment 1.1
    Video's for the three codes with execution.
    Screenshots of the 3 codes.

---
+++ Source Code/Program 1/ICP Program 1.py
@@ -0,0 +1,9 @@
+t = ("python") # t is assigned the string value of python
+print (t)
+t = (t[2::])
+print (t) #the print command print the result deleting the 2 characters from the string
+t = (t[:: -1]) # the command reverses the resultant string by printing it backwards starting from index -1
+print (t)
+
+
+
---
+++ Source Code/Program 2/ICP Program 2.py
@@ -0,0 +1,5 @@
+num1 = int(input ("enter first number:")) #input command to get a number from the user
+num2 = int(input ("enter second number:")) # second input command to get input number from the user
+add = num1 + num2 #to add two numbers after getting input from the user
+print ("sum of" , num1 , "and" , num2 , "is:" , add) # this command will add the num1 and num2 recieved from the user
+
